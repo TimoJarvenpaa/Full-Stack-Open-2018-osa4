29f6ef87d79985a96b52a02f4fa3386497bd883b
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2018-10-31 16:33:22 +0200

    Tehtävä 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,6 +5,14 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (previous, current) => {
+    return (previous.likes > current.likes) ? previous : current
+  }
+  return blogs.length === 0 ? {} : blogs.reduce(reducer)
+}
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
