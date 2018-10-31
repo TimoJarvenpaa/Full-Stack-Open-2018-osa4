afb5fbb4ebbfef502630f61571870e6d0a515611
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2018-10-31 15:02:27 +0200

    Tehtävä 4.2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-//const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -23,26 +23,13 @@ mongoose
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.logger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(middleware.error)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
